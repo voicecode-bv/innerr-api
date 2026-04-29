9a8c91607e3229425bbe9ac87f4a05330a780ca5
Michael Blijleven <info@voicecode.nl>
2026-04-29 15:13:25 +0200

    Updates

---
+++ resources/views/vendor/mail/html/message.blade.php
@@ -0,0 +1,34 @@
+<x-mail::layout>
+{{-- Head --}}
+<x-slot:head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600&family=Fraunces:opsz,SOFT,wght@9..144,0..100,100..900&display=swap" rel="stylesheet">
+</x-slot:head>
+
+{{-- Header --}}
+<x-slot:header>
+<x-mail::header :url="config('app.url')">
+{{ config('app.name') }}
+</x-mail::header>
+</x-slot:header>
+
+{{-- Body --}}
+{!! $slot !!}
+
+{{-- Subcopy --}}
+@isset($subcopy)
+<x-slot:subcopy>
+<x-mail::subcopy>
+{!! $subcopy !!}
+</x-mail::subcopy>
+</x-slot:subcopy>
+@endisset
+
+{{-- Footer --}}
+<x-slot:footer>
+<x-mail::footer>
+© {{ date('Y') }} {{ config('app.name') }}. {{ __('All rights reserved.') }}
+</x-mail::footer>
+</x-slot:footer>
+</x-mail::layout>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\DocumentationController;
+use Illuminate\Mail\Markdown;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {});
 
 Route::get('/api/docs', [DocumentationController::class, 'ui'])->name('api.docs');
 Route::get('/api/docs/openapi.json', [DocumentationController::class, 'spec'])->name('api.docs.spec');
@@ -13,3 +13,18 @@ Route::get('/api/docs/openapi.json', [DocumentationController::class, 'spec'])->
 Route::get('test', function () {
     auth()->loginUsingId(1);
 });
+
+Route::get('/mail/preview', function (): string {
+    $message = (new MailMessage)
+        ->subject(__('Welcome to Innerr'))
+        ->greeting(__('Hello!'))
+        ->line(__('This is a preview of the Innerr email template.'))
+        ->line(__('Headings below should render in Fraunces, body copy in DM Sans.'))
+        ->action(__('Open Innerr'), config('app.url'))
+        ->line(__('Thanks for being part of the circle.'));
+
+    return app(Markdown::class)->render(
+        $message->markdown ?? 'notifications::email',
+        $message->data(),
+    );
+})->name('mail.preview');
